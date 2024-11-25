79dd0c55a04a4d5144df3a65a1a4e2eea6098970
Caio Abe <caio.abe@estilo7.com.br>
2024-11-25 18:42:02 -0300

    rails g authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create! email_address: 'caio.abe@estilo7.com.br', password: "123"
